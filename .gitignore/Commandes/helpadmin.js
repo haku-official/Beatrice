b2fb4bc1a8e8168bfbf1c5fdfe7df0e7a48508fa
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-20 22:36:33 +0200

    Update helpadmin.js

--- .gitignore/Commandes/helpadmin.js
+++ .gitignore/Commandes/helpadmin.js
@@ -1,19 +1,18 @@
+
 var Discord = require('discord.js');
+var bot = new Discord.Client({ disableEveryone: false }, { autoReconnect: true });
+var fs = require('fs');
+
+bot.commands = new Discord.Collection();
+var commandsList = fs.readFileSync('storage/helpadmin.txt', 'utf8')
 
 exports.run = (bot, msg, args) => {
 
-    var embed = new Discord.RichEmbed()
-        .setColor("#33cccc")
-        .setAuthor("Histoire de Majiusekai")
-        .addField("Les débuts", "Le monde de Majiyusekai est un monde parallèle qui a été crée il y a des milliards d'années en même temps que la terre. Son accessibilité est due à une brèche temporels dans tous les mondes (mondes des humains, monde des esprits,...). Depuis qu'il y a des habitants dans ce monde, la paix perdure. Moi, Beatrice, je suis la protectrice de ce monde depuis toutes petites. Mes parents (esprits) protégeaient aussi ce monde. Toutes les surfaces du monde n'ont pas encore été découvertes. Les ancêtres auraient vécu là-bas...")
-        .setFooter("Beatrice Développé par FUU | V1.0 ")
-
-    msg.channel.send("chargement en cours..").then(m => m.edit(embed));
+    member.user.send(commandsList);
 
 };
 
 
-
 exports.help = {
-    name: 'hdeb'
+    name: 'helpadmin'
 };
