2979100b4d45b403eee76637187c675f6a483d9a
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-20 21:41:27 +0200

    Update Histoire.js

--- .gitignore/Commandes/Histoire.js
+++ .gitignore/Commandes/Histoire.js
@@ -5,7 +5,7 @@ exports.run = (bot, msg, args) => {
     var embed = new Discord.RichEmbed()
         .setColor("#33cccc")
         .setAuthor("Histoire de Majiusekai")
-        .addField("Les débuts", "Dans le monde des esprits, il y a 7ans, une guerre éclata. La plus puissante des esprits de l'eau (la mère de Fuu) créa un monde qui a pris le nom de Majiyusekai. Au début, il n'y avait que Fuu et moi puis,  se fut l'arrivée de Aiichirō. Plus le temps avançait et plus les personnes arrivèrent les unes après les autres. Des animaux, des humains, des esprits,... Ce monde était rempli d'harmonie.")
+        .addField("Les débuts", "Le monde de Majiyusekai est un monde parallèle qui a été crée il y a des milliards d'années en même temps que la terre. Son accessibilité est due à une brèche temporels dans tous les mondes (mondes des humains, monde des esprits,...). Depuis qu'il y a des habitants dans ce monde, la paix perdure. Moi, Beatrice, je suis la protectrice de ce monde depuis toutes petites. Mes parents (esprits) protégeaient aussi ce monde. Toutes les surfaces du monde n'ont pas encore été découvertes. Les ancêtres auraient vécu là-bas...")
         .setFooter("Beatrice Développé par FUU | V1.0 ")
 
     msg.channel.send("chargement en cours..").then(m => m.edit(embed));
