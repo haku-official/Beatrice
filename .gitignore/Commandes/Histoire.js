a7afca9abcedaf04c8c0e738441bd2a25b5192f9
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-21 20:56:01 +0200

    Update Histoire.js

--- .gitignore/Commandes/Histoire.js
+++ .gitignore/Commandes/Histoire.js
@@ -5,7 +5,7 @@ exports.run = (bot, msg, args) => {
     var embed = new Discord.RichEmbed()
         .setColor("#33cccc")
         .setAuthor("Histoire de Majiusekai")
-        .addField("Les débuts", "Le monde de Majiyusekai est un monde parallèle qui a été crée il y a des milliards d'années en même temps que la terre. Son accessibilité est due à une brèche temporels dans tous les mondes (mondes des humains, monde des esprits,...). Depuis qu'il y a des habitants dans ce monde, la paix perdure. Moi, Beatrice, je suis la protectrice de ce monde depuis toutes petites. Mes parents (esprits) protégeaient aussi ce monde. Toutes les surfaces du monde n'ont pas encore été découvertes. Les ancêtres auraient vécu là-bas...")
+        .addField("Le contexte", "Dans le monde de Majiyusekai, il y avait un peuple où les humains, les esprits,... vivaient en harmonies. Il se nommait le peuple de la nature. Il régnait sur le pays de Shizen. Dans ce pays, il y avait une multitude de forêts, de lac,... Mais il y avait, aux frontières, des zones inconnus de tous. Personnes ne les avaient explorés. Mais un jour un groupe d'aventurier, franchisèrent la frontière. Hélas personnes ne les revues...")
         .setFooter("Beatrice Développé par FUU | V1.0 ")
 
     msg.channel.send("chargement en cours..").then(m => m.edit(embed));
