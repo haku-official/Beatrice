7a4467c4597df3c555c004d41c8ac99a31f6be20
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-24 19:46:02 +0200

    Update helpadmin.js

--- .gitignore/Commandes/helpadmin.js
+++ .gitignore/Commandes/helpadmin.js
@@ -3,7 +3,7 @@ var bot = new Discord.Client({ disableEveryone: false }, { autoReconnect: true }
 var fs = require('fs');
 
 bot.commands = new Discord.Collection();
-var commandsList = fs.readFileSync('storage/help.txt', 'utf8')
+var commandsList = fs.readFileSync('storage/helpadmin.txt', 'utf8')
 
 exports.run = (bot, msg, args) => {
 
