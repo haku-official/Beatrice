33038e1d1ae2f12eb7f4f6880885525084444e0b
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-24 19:30:01 +0200

    Update help.js

--- .gitignore/Commandes/help.js
+++ .gitignore/Commandes/help.js
@@ -1,18 +1,19 @@
+
 var Discord = require('discord.js');
 var bot = new Discord.Client({ disableEveryone: false }, { autoReconnect: true });
 var fs = require('fs');
+
 bot.commands = new Discord.Collection();
 var commandsList = fs.readFileSync('storage/help.txt', 'utf8')
 
-module.exports.run = async (bot, message, args) => {
-
-   exports.run = (bot, msg, args) => {
+exports.run = (bot, msg, args) => {
 
     msg.channel.send(commandsList);
 
 };
 
 
+
 exports.help = {
-    name: 'help'
+    name: 'lois2'
 };
