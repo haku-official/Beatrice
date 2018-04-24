6dfe04d0c7a3bc718d963e57c377a1de26749d27
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-24 19:27:25 +0200

    Update help.js

--- .gitignore/Commandes/help.js
+++ .gitignore/Commandes/help.js
@@ -6,11 +6,12 @@ var commandsList = fs.readFileSync('storage/help.txt', 'utf8')
 
 module.exports.run = async (bot, message, args) => {
 
-       try {
-        await tomute.send(`Tu a été rendu muet pendant ${mutetime}.`)
-    } catch (e) {
-        message.channel.send(`A user has been muted... but their DMs are locked. They will be muted for ${mutetime}`)
-    }
+   exports.run = (bot, msg, args) => {
+
+    msg.channel.send(commandsList);
+
+};
+
 
 };
 
