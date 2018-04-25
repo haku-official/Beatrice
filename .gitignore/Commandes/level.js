6eb5444523df721c7365a3d5b3d1ff688cf6ee54
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-25 12:33:09 +0200

    Update level.js

--- .gitignore/Commandes/level.js
+++ .gitignore/Commandes/level.js
@@ -1,4 +1,3 @@
-
 const Discord = require("discord.js");
 const botconfig = require("../botconfig");
 let purple = botconfig.purple;
@@ -6,27 +5,28 @@ let xp = require("../xp.json");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!xp[message.author.id]){
-   xp[message.author.id] = {
-     xp: 0,
-     level: 1
-  };
-}
-  let curxp = xp[message.author.id].xp;
-  let curlvl = xp[message.author.id].level;
-  let nxtLvlXp = curlvl * 300;
-  let difference = nxtLvlXp - curxp;
+    if (!xp[message.author.id]) {
+        xp[message.author.id] = {
+            xp: 0,
+            level: 1
+        };
+    }
+    let curxp = xp[message.author.id].xp;
+    let curlvl = xp[message.author.id].level;
+    let nxtLvlXp = curlvl * 300;
+    let difference = nxtLvlXp - curxp;
 
-  let lvlEmbed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor(purple)
-  .addField("Level", curlvl, true)
-  .addField("XP", curxp, true)
-  .setFooter(`${difference} XP est tu level up !`, message.author.displayAvatarURL);
+    let lvlEmbed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setColor("#33cccc")
+        .addField("Level", curlvl, true)
+        .addField("XP", curxp, true)
+        .setFooter(`${difference} XP est tu Level Up`, message.author.displayAvatarURL);
 
-  message.channel.send(lvlEmbed);
+    message.channel.send(lvlEmbed).then;
+
 }
 
 module.exports.help = {
-  name: "level"
+    name: "level"
 }
