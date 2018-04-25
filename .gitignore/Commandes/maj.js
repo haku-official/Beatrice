e7ca708094c640229d8160fb95a0b7791bc136ee
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-25 17:05:15 +0200

    Update maj.js

--- .gitignore/Commandes/maj.js
+++ .gitignore/Commandes/maj.js
@@ -3,7 +3,7 @@ var bot = new Discord.Client({ disableEveryone: false }, { autoReconnect: true }
 var fs = require('fs');
 
 bot.commands = new Discord.Collection();
-var commandsList = fs.readFileSync('storage/MAJ.txt', 'utf8')
+var commandsList = fs.readFileSync('storage/Maj.txt', 'utf8')
 
 exports.run = (bot, msg, args) => {
 
