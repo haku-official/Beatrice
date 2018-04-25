4225b51ec43d186cca75a3efa9df838c5a17fd9d
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-25 11:35:37 +0200

    Update Fiche.js

--- .gitignore/Commandes/Fiche.js
+++ .gitignore/Commandes/Fiche.js
@@ -15,12 +15,13 @@ exports.run = (bot, msg, args) => {
         .addField("Poids", "62kg")
         .addField("Orientation sexuelle", "Bisexuelle")
         .addField("Races", "Oni")
-        .addField("Personnalité", "Garçon très facile, il est assez cool dans la vie de tous les jours et n’aime vraiment pas se prendre la tête avec les autres. Il sourit toujours et son sourire fait sa bonne humeur. Malgré le fait qu’il se relâche trop, il adopte un côté sombre lorsque l’on s’approche trop près des personnes qu’il aime et peu se montrer violent si on leur veut du mal. L’on pourrait dire que c’est un « faux-calme ». Les responsabilités ne sont pas faites pour lui et préfère laisser les autres s’en occuper. Bien malgré son côté feignant, quand il le veut, il peut être une personne qui met beaucoup de cœur et d’intérêt dans son travail.")
-        .addField("Pouvoir", "La maîtrise du Vent , il en fait ce qu'il veut")
+        .addField("Personnalité", "Est quelqu'un qui adore se faire des amies et rencontré des personnes")
+        .addField("Pouvoir", "Energie spirituelle: Peut contrôler au maximum 2 objet grâce a cette magie. Elle peut faire a peu près tout ce qu'elle veux de l'objet et peux retire son contrôle quand elle veux. Les deux énergie qu'elle contrôle sont stocké dans les deux boulles rouge de son vêtement ce qui peut être servit aussi comme arme. Force suroni: La Force Suroni est quelque chose de particulier ayant déjà la force d'une oni elle peut encore décuplé sa force mais cela que sous la condition qu'elle est un fort taux d'alcool dans le sang. Gōruden'ai: La définition de yeux dorée c'est un pouvoir particulier qui permet de donné l'illusion qu'elle veux a quelqu'un. ")
         .addField("Points Faibles", "La sécheresse et les lieux sans air")
-        .addField("Histoire", "Il est né à Kyoto au Japon , il as grandis dans un bonne famille , plutôt aisé , mais à la découverte de ses pouvoirs tout à changé , il suivait des cours intensif pour son entrainement , chaque jour il répétait les même geste , mais un jour ses parents furent attaquer par une sorte de monstre hybrides Aiichirō essaya de faire ce qu'il pouvait en utilisant ses pouvoirs mais en vain , ce' monstre était trop puissant , Aiichirō vu alors ses Parents mourir sous ses yeux..Aujourd'hui il essaye de vivre avec ce lourd fardeaux qui pèse sur lui , il est quelque peu renfermer sur lui même et à du mal à parler avec des inconnus ..Il n'as pas vraiment eu de petites copines dans sa vie alors sentimentalement , il a du mal..")
-        .setImage('https://cdn.discordapp.com/attachments/427453254197182464/435394964332216322/Aronn_Btight_6.jpg')
-        .setFooter("Beatrice Développé par FUU | V1.0 ")
+        .addField("Histoire", "A toujours était une grande buveuse dans les tavernes d'ou sont surnom d'Oni Shiro Nomu hito c'est a dire l'Oni Blanche buveuse. Elle aime bien bavardé avec les inconnues et récolte toute sorte d'infos sur ce qu'il se trame partout même si elle a l'air de juste vouloir parlé elle reste tout du moins quelqu'un qui aime l'inconnue. Le reste elle vous le raconteras en rp.")
+        .setImage('https://cdn.discordapp.com/attachments/427453254197182464/438488948344487946/shinatobe_fearless_night_drawn_by_spike_wible__sample-2c6c02f6e633e2cf1569b54ccef335c6.png')
+        .setImage('https://cdn.discordapp.com/attachments/427453254197182464/438489021212000267/shinatobe_fearless_night_drawn_by_spike_wible__sample-d31db8a228ef3b2a6ead4432a6d4e962.png')
+	.setFooter("Beatrice Développé par FUU | V1.0 ")
         .setTimestamp()
 
 
@@ -33,5 +34,5 @@ exports.run = (bot, msg, args) => {
 
 
 exports.help = {
-	name: 'FicheChiro'
+	name: 'Fiche'
 };
