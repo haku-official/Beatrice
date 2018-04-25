b93a26be65485fb76b5499479c9dcc1ead616cf6
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-25 17:04:51 +0200

    Create maj.js

---
+++ .gitignore/Commandes/maj.js
@@ -0,0 +1,18 @@
+var Discord = require('discord.js');
+var bot = new Discord.Client({ disableEveryone: false }, { autoReconnect: true });
+var fs = require('fs');
+
+bot.commands = new Discord.Collection();
+var commandsList = fs.readFileSync('storage/MAJ.txt', 'utf8')
+
+exports.run = (bot, msg, args) => {
+
+    msg.channel.send(commandsList);
+
+};
+
+
+
+exports.help = {
+    name: 'maj'
+};
