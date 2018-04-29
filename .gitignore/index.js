5fbd1d882cdb751ce0b9c28c319fa56ce9ee3e1b
haku-official <37959296+haku-official@users.noreply.github.com>
2018-04-29 18:24:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -62,17 +62,39 @@ bot.on("ready", async () => {
    if (message.content.startsWith(prefix)) {
         let commandfile = bot.commands.get(cmd.slice(prefix.length));
         if(commandfile) 
-        
-      
-    {
-      
-        
-      
-        commandfile.run(bot,message,args);
+        {
+            commandfile.run(bot,message,args);
+        } else {
+            message.reply("```❌| cette commande n'existe pas.```");  
+        }
     } else {
-        message.reply("```❌| cette commande n'existe pas.```");  
+        coins = require("./coins.json");
+        if (!coins[message.author.id]) {
+            coins[message.author.id] = {
+                coins: 0
+            };
+        }
+
+        let coinAmt = Math.floor(Math.random() * 15) + 1;
+        let baseAmt = Math.floor(Math.random() * 15) + 1;
+        console.log(`${coinAmt} ; ${baseAmt}`);
+
+        if (coinAmt === baseAmt) {
+            coins[message.author.id] = {
+                coins: parseInt(coins[message.author.id].coins) + coinAmt
+            };
+            fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+                if (err) console.log(err)
+            });
+            let coinEmbed = new Discord.RichEmbed()
+                .setAuthor(message.author.username)
+                .setColor("#33cccc")
+                .addField("💸", `${coinAmt} coins gagné`);
+
+            message.channel.send(coinEmbed).then(msg => { msg.delete(5000) });
         }
     }
+      
   });
 
 bot.on("guildMemberAdd", (member) => {
@@ -126,31 +148,6 @@ bot.on("guildMemberRemove", (member) => {
 })
 
 bot.on("message", async message => {
-    coins = require("./coins.json");
-    if (!coins[message.author.id]) {
-        coins[message.author.id] = {
-            coins: 0
-        };
-    }
-
-    let coinAmt = Math.floor(Math.random() * 15) + 1;
-    let baseAmt = Math.floor(Math.random() * 15) + 1;
-    console.log(`${coinAmt} ; ${baseAmt}`);
-
-    if (coinAmt === baseAmt) {
-        coins[message.author.id] = {
-            coins: parseInt(coins[message.author.id].coins) + coinAmt
-        };
-        fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-            if (err) console.log(err)
-        });
-        let coinEmbed = new Discord.RichEmbed()
-            .setAuthor(message.author.username)
-            .setColor("#33cccc")
-            .addField("💸", `${coinAmt} coins gagné`);
-
-        message.channel.send(coinEmbed).then(msg => { msg.delete(5000) });
-    }
 
     if (message.author.bot) return;
     if (message.channel.type === "dm") return;
