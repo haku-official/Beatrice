d61eb237c7a80b67ec3d649877f2124c84b1c9cf
haku-official <37959296+haku-official@users.noreply.github.com>
2018-05-01 10:40:47 +0200

    Update role3.js

--- .gitignore/Commandes/role3.js
+++ .gitignore/Commandes/role3.js
@@ -18,10 +18,6 @@ exports.run = (client, message, args) => {
     message.delete().catch(O_o => { });
     message.channel.send("```Vous avez obtenu(e) le rôle |🎥GhibliFan🎥|```").then(msg => {msg.delete(5000)});
 
-    let enterembed = new Discord.RichEmbed()
-        .setDescription(`${message.author} est entré dans le monde`)
-        .setColor("#33cccc")
-        
 
     let incidentschannel = message.guild.channels.find(`name`, "général");
     if (!incidentschannel) return message.reply("Please create a incidents channel first!");
