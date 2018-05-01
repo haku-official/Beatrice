7e38d0e89a33c71d4392b46f032e6cd8cccff96c
haku-official <37959296+haku-official@users.noreply.github.com>
2018-05-01 10:24:16 +0200

    Update role2.js

--- .gitignore/Commandes/role2.js
+++ .gitignore/Commandes/role2.js
@@ -1,1 +1,34 @@
+const Discord = require('discord.js');
 
+exports.run = (client, message, args) => {
+
+    var osu = message.guild.roles.find(`name`, "|🗾JapanFan🗾|");//ajouter
+    //____________________________________________
+    
+
+    //___________________________________________________________________________
+
+    try {
+        message.member.addRole(osu);
+        console.log("Le role " + osu.name + " a bien ajouté à " + message.author.username + " !");
+    }
+    catch (err) {
+        console.log("Le role " + osu.name + " n'a pas pu être ajouté à " + message.author.username + " !");
+    }
+    message.delete().catch(O_o => { });
+    message.channel.send("```Vous avez obtenu(e) le rôle |🗾JapanFan🗾|```")
+
+    let enterembed = new Discord.RichEmbed()
+        .setDescription(`${message.author} est entré dans le monde`)
+        .setColor("#33cccc")
+        
+
+    let incidentschannel = message.guild.channels.find(`name`, "général");
+    if (!incidentschannel) return message.reply("Please create a incidents channel first!");
+    incidentschannel.send(enterembed);
+    
+};
+
+module.exports.help = {
+    name: 'JFan'
+};
