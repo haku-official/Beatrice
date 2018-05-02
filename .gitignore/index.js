d3b16924da81d2511cc8ee431c78ffdc593c9c7c
haku-official <37959296+haku-official@users.noreply.github.com>
2018-05-02 16:56:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -49,53 +49,7 @@ bot.on("ready", async () => {
   //------------------------------------------------------------
   
 //bot.on('message', censure => {
-  bot.on("message" , async message => {
-    
-    
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-  
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-   if (message.content.startsWith(prefix)) {
-        let commandfile = bot.commands.get(cmd.slice(prefix.length));
-        if(commandfile) 
-        {
-            commandfile.run(bot,message,args);
-        } else {
-            message.reply("```❌| cette commande n'existe pas.```");  
-        }
-    } else {
-        if (!coins[message.author.id]) {
-            coins[message.author.id] = {
-                coins: 0
-            };
-        }
-
-        let coinAmt = Math.floor(Math.random() * 15) + 1;
-        let baseAmt = Math.floor(Math.random() * 15) + 1;
-        console.log(`${coinAmt} ; ${baseAmt}`);
-
-        if (coinAmt === baseAmt) {
-            coins = require("./coins.json");
-            coins[message.author.id] = {
-                coins: parseInt(coins[message.author.id].coins) + coinAmt
-            };
-            fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-                if (err) console.log(err)
-            });
-            let coinEmbed = new Discord.RichEmbed()
-                .setAuthor(message.author.username)
-                .setColor("#33cccc")
-                .addField("💸", `${coinAmt} coins gagné`);
-
-            message.channel.send(coinEmbed).then(msg => { msg.delete(5000) });
-        }
-    }
-      
-  });
+ 
 
 bot.on("guildMemberAdd", (member) => {
     if (member.guild.channels.has("438455650989899796")) {
